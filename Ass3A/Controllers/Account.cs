44f05b117393f672d1e345a6508094fedcb1d9ed
Godz <Godz@GOD>
2022-07-14 14:15:00 +1000

    Add project files.

---
+++ Ass3A/Controllers/Account.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Ass3A.Controllers
+{
+    public class Account : Controller
+    {
+        /*public IActionResult Index()
+        {
+            return View();
+        }
+        */
+        public IActionResult Index()
+        {
+            return View();
+            ;
+        }
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+    }
+}
---
+++ Ass3A/Controllers/Seminar.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.IO;
+using System.Web;
+namespace Ass3A.Controllers
+{
+    public class Seminar : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
